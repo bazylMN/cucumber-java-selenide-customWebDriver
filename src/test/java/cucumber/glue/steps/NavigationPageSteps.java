eae3f11ce5927581d14a47827fe03462f3c2be74
bazylMN <m.napieralska.smutniak@gmail.com>
2019-04-19 11:16:49 +0200

    unused En

--- src/test/java/cucumber/glue/steps/NavigationPageSteps.java
+++ src/test/java/cucumber/glue/steps/NavigationPageSteps.java
@@ -2,11 +2,10 @@ package cucumber.glue.steps;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import cucumber.api.java8.En;
 import cucumber.glue.pageObjects.NavigationPageObjects;
 import org.junit.Assert;
 
-public class NavigationPageSteps implements En {
+public class NavigationPageSteps {
 
     private NavigationPageObjects navigationPageObjects;
 
